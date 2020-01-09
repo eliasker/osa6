2e0af9f988a21736edd116bdd571fa7bf75f356c
Elias <eliasker@gmail.com>
2020-01-09 19:23:34 +0200

    vote & add new

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,33 +1,44 @@
 import React from 'react';
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
+	const anecdotes = props.store.getState()
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+	const vote = (id) => {
+		props.store.dispatch({ type: 'like', data: { id } })
+	}
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+	const createAnecdote = (event) => {
+		event.preventDefault()
+		const anecdote = event.target.anecdote.value
+		props.store.dispatch({
+			type: 'add',
+			data: {
+				anecdote: anecdote
+			}
+		})
+	}
+
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{anecdotes.map(anecdote =>
+				<div key={anecdote.id}>
+					<div>
+						{anecdote.content}
+					</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => vote(anecdote.id)}>vote</button>
+					</div>
+				</div>
+			)}
+			<h2>create new</h2>
+			<form onSubmit={createAnecdote}>
+				<div><input name="anecdote" /></div>
+				<button type="submit">create</button>
+			</form>
+		</div>
+	)
 }
 
 export default App
